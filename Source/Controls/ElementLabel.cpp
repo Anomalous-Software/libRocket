e14bbf61c0cd6499d2fe2597eebf0ce44dfabb91
Andrew Piper <andrewpiper@anomalousmedical.com>
2016-01-29 12:07:29 -0500

    Made it so element labels can have for attributes.

--- Source/Controls/ElementLabel.cpp
+++ Source/Controls/ElementLabel.cpp
@@ -1,4 +1,5 @@
 #include "../../Include/Rocket/Controls/ElementLabel.h"
+#include "../../Include/Rocket/Core/ElementDocument.h"
 
 namespace Rocket {
 namespace Controls {
@@ -17,18 +18,29 @@ void ElementLabel::ProcessEvent(Core::Event& event)
 	// Detect click events
 	if (event.GetTargetElement() == this &&	(event == "click"))
 	{
-		//Note that we have to loop since the ElementFormControlInput class does not pass its OnChildAdded to the superclass.
-		//We don't want to modify things too much, so we will just loop when clicked searching for the child input, not really
-		//a big deal.
-		int childCount = this->GetNumChildren();
-		Core::Element* child;
-		for (int i = 0; i < childCount; ++i)
+		if (this->HasAttribute("for"))
 		{
-			child = this->GetChild(i);
-			if (child->GetTagName() == "input")
+			Core::Element* forElement = this->GetOwnerDocument()->GetElementById(this->GetAttribute<Core::String>("for", ""));
+			if (forElement != NULL)
 			{
-				child->ProcessEvent(event);
-				i = childCount; //break loop
+				forElement->ProcessEvent(event);
+			}
+		}
+		else
+		{
+			//Note that we have to loop since the ElementFormControlInput class does not pass its OnChildAdded to the superclass.
+			//We don't want to modify things too much, so we will just loop when clicked searching for the child input, not really
+			//a big deal.
+			int childCount = this->GetNumChildren();
+			Core::Element* child;
+			for (int i = 0; i < childCount; ++i)
+			{
+				child = this->GetChild(i);
+				if (child->GetTagName() == "input")
+				{
+					child->ProcessEvent(event);
+					i = childCount; //break loop
+				}
 			}
 		}
 	}
